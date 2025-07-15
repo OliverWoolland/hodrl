213fbfb26159f1ffc5c9ce4c6d7cf5df3c8616e2
Oliver Woolland <oliver.woolland@manchester.ac.uk>
2025-07-15 09:10:49 +0100

    Give api a main function

--- hodrl/api.py
+++ hodrl/api.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+def main():
+    uvicorn.run("hodrl.api:app",
+                host="0.0.0.0",
+                port=8000,
+                workers=4,
+                reload=True)
+
+if __name__ == "__main__":
+    main()
