b712fff54291fbc1edfd77c7a4a49d5fc6e9e9d9
Oliver Woolland <oliver.woolland@manchester.ac.uk>
2025-07-17 08:24:32 +0100

    Add endpoint for getting all assets

--- src/hodrl/api.py
+++ src/hodrl/api.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 import uvicorn
+
+from hodrl import asset_queries
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/assets")
+async def get_assets():
+    assets = asset_queries.get_all_assets()
+    result = JSONResponse(content=assets, status_code=200)
+    return result
 
 def main():
     uvicorn.run("hodrl.api:app",
